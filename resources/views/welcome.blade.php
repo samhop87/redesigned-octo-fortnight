f21e973da014608066145e1bc6af8afdf4adb579
Sam Hopkinson <sam.hopkinson@purrgroup.com>
2021-06-02 11:56:04 +0100

    Reverted change to deployment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    BIG OL' TEST
+                                    BIG OL' TEST2
                                 </div>
                             </div>
                         </div>
