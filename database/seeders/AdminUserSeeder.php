abbfee22d6f63071284e898cb14a23dbcbba039c
Sam Hopkinson <sam.hopkinson@purrgroup.com>
2021-06-02 11:33:58 +0100

    Added authentication scaffolding

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Company;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'first_name' => 'Admin',
+            'last_name' => 'User',
+            'email' => 's.hopkinson87@gmail.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]);
+    }
+}
