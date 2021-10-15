8e3075bd8eb7d15d38afadc981ddae8e4dc394fc
Sam  Hopkinson <sam.hopkinson@purrgroup.com>
2021-10-15 10:29:05 +0100

    On the off chance this works

--- public/js/app.js
+++ public/js/app.js
@@ -2682,11 +2682,11 @@ window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__.default({
   broadcaster: 'pusher',
   // key: process.env.MIX_PUSHER_APP_KEY,
   // cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-  // forceTLS: true
+  forceTLS: true,
   key: 'your-pusher-key',
   wsHost: window.location.hostname,
   wsPort: 6001,
-  forceTLS: false,
+  // forceTLS: false,
   disableStats: true
 });
 
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -37,10 +37,10 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     // key: process.env.MIX_PUSHER_APP_KEY,
     // cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    // forceTLS: true
+    forceTLS: true,
     key: 'your-pusher-key',
     wsHost: window.location.hostname,
     wsPort: 6001,
-    forceTLS: false,
+    // forceTLS: false,
     disableStats: true,
 });
