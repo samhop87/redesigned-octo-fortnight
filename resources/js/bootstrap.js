347fa70d18ad0da912ed5fb925d8b777f77f9e69
Sam  Hopkinson <sam.hopkinson@purrgroup.com>
2021-10-17 13:34:43 +0100

    Changed config opton

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -35,12 +35,9 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    // key: process.env.MIX_PUSHER_APP_KEY,
-    // cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    forceTLS: true,
+    forceTLS: false,
     key: 'your-pusher-key',
     wsHost: window.location.hostname,
     wsPort: 6001,
-    // forceTLS: false,
     disableStats: true,
 });
